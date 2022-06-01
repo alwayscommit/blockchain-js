4859986818c0f33e7a8eedd8f685cec29c4496c9
"alwayscommit" <"ranglaniaakash.git@gmail.com">
2022-06-01 12:42:27 +0100

    call, send, transfer

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -53,7 +53,28 @@ contract FundMe {
 
     //whoever is the message sender, withdraw funds back to their account
     function withdraw() payable onlyOwner public {
+        //cast to payable address
+        //msg.sender = address
+        
+        //transfer
+        //payable(msg.sender) = payable address
         payable(msg.sender).transfer(address(this).balance);
+
+
+        //send
+        //transfer automatically reverts the transaction whereas send function needs extra handling
+        bool isSuccess = payable(msg.sender).send(address(this).balance);
+        require(isSuccess, "Send failed!");
+
+
+        //call - lower level command, can be used to call any function in Ethereum without the ABI
+        (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed!");
+
+        //transfer - 2300 gas, throws error
+        //send - 2300 gas, returns bool
+        //call - forward all gas or set gas, returns bool (recommended way)
+
         //once people have funded the smart contract/project, we can withdraw everything and update their balances to 0
         for(uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
