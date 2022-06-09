5f437a99a46241da61822460fff992775d1534c8
alwayscommit <ranglaniaakash.git@gmail.com>
2022-06-09 17:10:53 +0100

    contract verification used deploy.js

---
+++ hardhat-simplestorage-fcc/scripts/deploy.js
@@ -0,0 +1,40 @@
+const { ethers, run, network } = require("hardhat")
+
+async function main() {
+  //hardhat knows about the contract folder and knows it is already compiled
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+  console.log("Deploying contract...")
+  //deploys to hardhat network
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  await simpleStorage.deployed()
+  console.log(`Deployed Contract to: ${simpleStorage.address}`)
+  //don't want to call the verify function when we deploy to hardhat
+  // console.log(network.config)
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    await simpleStorage.deployTransaction.wait(6)
+    await verify(simpleStorage.address, [])
+  }
+}
+
+async function verify(contractAddress, args) {
+  console.log("Verifying contract...")
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase.includes("already verified")) {
+      console.log("Already Verified")
+    } else {
+      console.log(e)
+    }
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
