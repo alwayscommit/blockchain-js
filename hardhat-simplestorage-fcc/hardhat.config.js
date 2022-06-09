cbfc10e5916f9543c25ca239d6d346fd4d2c2118
alwayscommit <ranglaniaakash.git@gmail.com>
2022-06-09 18:50:06 +0100

    test coverage setup

--- hardhat-simplestorage-fcc/hardhat.config.js
+++ hardhat-simplestorage-fcc/hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block-number")
 require("hardhat-gas-reporter")
+require("solidity-coverage")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,10 +21,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-const CMC_API_KEY = process.env.CMC_API_KEY
+const RINKEBY_RPC_URL =
+  process.env.RINKEBY_RPC_URL || "https://eth-rinkeby/example"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "something"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "something"
+const CMC_API_KEY = process.env.CMC_API_KEY || "something"
 
 module.exports = {
   defaultNetwork: "hardhat",
