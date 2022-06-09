6f7c25827b8974d4a0a04fee9e2bd519863d17d7
alwayscommit <ranglaniaakash.git@gmail.com>
2022-06-09 18:18:18 +0100

    another test case

--- hardhat-simplestorage-fcc/test/test-deploy.js
+++ hardhat-simplestorage-fcc/test/test-deploy.js
@@ -17,4 +17,12 @@ describe("SimpleStorage", function () {
     const expectedValue = "0"
     assert.equal(currentValue.toString(), expectedValue)
   })
+
+  it("Should update when we call store", async function () {
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const expectedValue = "7"
+    const currentValue = await simpleStorage.retrieve()
+    assert.equal(currentValue.toString(), expectedValue)
+  })
 })
