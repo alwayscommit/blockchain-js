83bb15e846fd7702105fa42b8fa75b1fdd6cf53f
alwayscommit <ranglaniaakash.git@gmail.com>
2022-06-11 16:33:55 +0100

    fundme test case - before funding, after withdraw

--- hardhat-fund-me-fcc/test/unit/FundMe.test.js
+++ hardhat-fund-me-fcc/test/unit/FundMe.test.js
@@ -5,6 +5,7 @@ describe("FundMe", async function () {
     let fundMe
     let deployer
     let mockV3Aggregator
+    const sendValue = ethers.utils.parseEther("1") //ETH to wei representation
 
     beforeEach(async function () {
         //deploy FundMe contract
@@ -44,6 +45,60 @@ describe("FundMe", async function () {
         it("Fails if you don't send enough ETH", async function () {
             await expect(fundMe.fund()).to.be.revertedWith(
                 "You need to fund more ETH!"
+            )
+        })
+        it("updates the amount funded data structure", async function () {
+            await fundMe.fund({ value: sendValue })
+            const response = await fundMe.addressAmountMap(deployer)
+            assert.equal(response.toString(), sendValue.toString())
+        })
+        it("updates adds funder to array of funders", async function () {
+            await fundMe.fund({ value: sendValue })
+            const response = await fundMe.funders(0)
+            assert.equal(response.toString(), deployer)
+        })
+    })
+
+    describe("withdraw", async function () {
+        beforeEach(async function () {
+            await fundMe.fund({ value: sendValue })
+        })
+
+        it("withdraw ETH from a single founder", async function () {
+            //arrange
+            //get starting balance of the deployer and the contract
+            //here we could even do ethers.provider.getBalance(fundMe.address)
+            //but here we used provider of the fundMe contract, doesn't really matter
+            //we want to use the getBalance() function of the provider object that gets
+            //the balance of any contract
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+            //act
+
+            const transactionResponse = await fundMe.withdraw()
+            //wait till the transaction completes and then check whether all the contract balance is
+            //send to the owner's address
+            const transactionReceipt = await transactionResponse.wait(1)
+            const { gasUsed, effectiveGasPrice } = transactionReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+            //assert
+            assert.equal(endingFundMeBalance, 0)
+            assert.equal(
+                //BigNumber.add() function is better to perform math operations, makes it easier
+                startingFundMeBalance.add(startingDeployerBalance),
+                endingDeployerBalance.add(gasCost).toString()
             )
         })
     })
